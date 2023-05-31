06f8434c51e8bbeac8dca9dd1afdbde66aa03863
December81 <megastudio@outlook.fr>
2023-05-31 13:23:18 +0200

    API citations generator Vue JS app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Générateur de Citations</h1>
+    <button v-on:click="generateQuote">Générer une citation</button>
+    <p>{{ quote }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      quote: '',
+    }
+  },
+  methods: {
+    generateQuote() {
+      axios.get('https://type.fit/api/quotes')
+        .then(response => {
+          const quotes = response.data;
+          const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+          this.quote = `${randomQuote.text} - ${randomQuote.author}`;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 }
 </script>
@@ -22,5 +39,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  background-color: #42b983;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+  padding: 10px 20px;
+  user-select: none;
 }
 </style>
